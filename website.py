4452fcf0ad851149df6f00fea0e49100c904cd61
Kiran Jonnalagadda <jace@pobox.com>
2010-10-06 23:21:04 +0530

    Updated schedule, removed registration form and call for sponsors, and upgraded backend, but the latter far too late to take live.

--- website.py
+++ website.py
@@ -10,11 +10,12 @@ from datetime import datetime
 from uuid import uuid4
 from base64 import b64encode
 from flask import Flask, abort, request, render_template, redirect, url_for
+from flask import flash, session, g
 from werkzeug import generate_password_hash, check_password_hash
 from flaskext.sqlalchemy import SQLAlchemy
 from flaskext.mail import Mail, Message
-from flaskext.wtf import Form, TextField, TextAreaField
-from flaskext.wtf import Required, Email
+from flaskext.wtf import Form, TextField, TextAreaField, PasswordField
+from flaskext.wtf import Required, Email, ValidationError
 from pytz import utc, timezone
 from markdown import markdown
 try:
@@ -89,6 +90,28 @@ def newid():
     """
     return b64encode(uuid4().bytes, altchars=',-').replace('=', '')
 
+
+def currentuser():
+    """
+    Get the current user, or None if user isn't logged in.
+    """
+    if 'userid' in session and session['userid']:
+        return User.query.filter_by(email=session['userid']).first()
+    else:
+        return None
+
+
+def getuser(f):
+    """
+    Decorator for routes that need a logged-in user.
+    """
+    def wrapped(*args, **kw):
+        g.user = currentuser()
+        return f(*args, **kw)
+    wrapped.__name__ = f.__name__
+    return wrapped
+
+
 # ---------------------------------------------------------------------------
 # Data models and forms
 
@@ -147,11 +170,17 @@ class User(db.Model):
     #: Email id (repeated from participant.email, but unique here)
     email = db.Column(db.Unicode(80), nullable=False, unique=True)
     #: Private key, for first-time access without password
-    privatekey = db.Column(db.String(22), nullable=False, default=newid)
+    privatekey = db.Column(db.String(22), nullable=False, unique=True, default=newid)
     #: Public UID; not clear what this could be used for
-    uid = db.Column(db.String(22), nullable=False, default=newid)
+    uid = db.Column(db.String(22), nullable=False, unique=True, default=newid)
     #: Password hash
     pw_hash = db.Column(db.String(80))
+    #: Is this account active?
+    active = db.Column(db.Boolean, nullable=False, default=False)
+    #: Date of creation
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #: Date of first login
+    firstuse_date = db.Column(db.DateTime, nullable=True)
 
     def _set_password(self, password):
         if password is None:
@@ -177,22 +206,111 @@ class RegisterForm(Form):
     reason = TextAreaField('Your reasons for attending', validators=[Required()])
 
 
+class LoginForm(Form):
+    email = TextField('Email address', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+
+    def getuser(self, name):
+        return User.query.filter_by(email=name).first()
+
+    def validate_username(self, field):
+        existing = self.getuser(field.data)
+        if existing is None:
+            raise ValidationError, "No user account for that email address"
+        if not existing.active:
+            raise ValidationError, "This user account is disabled"
+
+    def validate_password(self, field):
+        user = self.getuser(self.email.data)
+        if user is None or not user.check_password(field.data):
+            raise ValidationError, "Incorrect password"
+        self.user = user
+
+
 # ---------------------------------------------------------------------------
 # Routes
 
 @app.route('/', methods=['GET'])
-def index():
-    regform = RegisterForm()
-    return render_template('index.html', regform=regform, gallery_sections=GALLERY_SECTIONS)
+@getuser
+def index(**forms):
+    regform = forms.get('regform', RegisterForm())
+    loginform = forms.get('loginform', LoginForm())
+    return render_template('index.html',
+                           regform=regform,
+                           loginform=loginform,
+                           gallery_sections=GALLERY_SECTIONS)
+
+
+@app.route('/login')
+def loginkey():
+    """
+    Login (via access key only)
+    """
+    key = request.args.get('key')
+    if key is None:
+        return redirect(url_for('index'), code=303)
+    user = User.query.filter_by(privatekey=key).first()
+    if user is None:
+        flash("Invalid access key", 'error')
+        return redirect(url_for('index'), code=303)
+    if not user.active:
+        flash("This account is disabled", 'error')
+        return redirect(url_for('index'), code=303)
+    if user.pw_hash != '':
+        # User already has a password. Can't login by key now.
+        flash("This access key is not valid anymore", 'error')
+        return redirect(url_for('index'), code=303)
+    if user.firstuse_date is None:
+        user.firstuse_date = datetime.utcnow()
+        db.session.commit()
+    g.user = user
+    session['userid'] = user.email
+    flash("You are now logged in", 'info')
+    return redirect(url_for('index'), code=303)
+
+
+@app.route('/logout')
+def logout():
+    g.user = None
+    del session['userid']
+    return redirect(url_for('index'), code=303)
+
+
+@app.route('/rsvp')
+def rsvp():
+    key = request.args.get('key')
+    choice = request.args.get('rsvp')
+    if key is None:
+        flash(u"You need an access key to RSVP.", 'error')
+        return redirect(url_for('index'), code=303)
+    if choice not in ['Y', 'N', 'M']:
+        flash(u"You need to RSVP with Yes, No or Maybe: Y, N or M.", 'error')
+        return redirect(url_for('index'), code=303)
+    user = User.query.filter_by(privatekey=key).first()
+    if user is None:
+        flash(u"Sorry, that access key is not in our records.", 'error')
+        return redirect(url_for('index'), code=303)
+    user.participant.rsvp = choice
+    if choice == 'Y':
+        flash(u"Yay! So glad you will be joining us.", 'info')
+    elif choice == 'N':
+        flash(u"Sorry you can't make it. Hope you’ll join us next time.", 'error') # Fake 'error' for frowny icon
+    elif choice == 'M':
+        flash(u"We recorded you as Maybe Attending. When you know better, could you select Yes or No?", 'info')
+    db.session.commit()
+    return redirect(url_for('index'), code=303)
+
 
 @app.route('/favicon.ico')
 def favicon():
     return redirect(url_for('static', filename='favicon.ico'), code=301)
 
+
 @app.route('/robots.txt')
 def robots():
     # Disable support for indexing fragments, since there's no backing code
     return "Disallow: /*_escaped_fragment_\n"
+
 
 # ---------------------------------------------------------------------------
 # Form submission
@@ -200,6 +318,19 @@ def robots():
 @app.route('/', methods=['POST'])
 def submit():
     # There's only one form, so we don't need to check which one was submitted
+    formid = request.form.get('form.id')
+    if formid == 'regform':
+        return submit_register()
+    elif formid == 'login':
+        return submit_login()
+    else:
+        flash("Unknown form", 'error')
+        redirect(url_for('index'), code=303)
+
+
+def submit_register():
+    # This function doesn't need parameters because Flask provides everything
+    # via thread globals.
     form = RegisterForm()
     if form.validate_on_submit():
         participant = Participant()
@@ -213,7 +344,28 @@ def submit():
             return render_template('regform.html',
                                    regform=form, ajax_re_register=True)
         else:
-            return render_template('index.html', regform=form)
+            flash("Please check your details and try again.", 'error')
+            return index(regform=form)
+
+
+def submit_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = form.user
+        g.user = user
+        session['userid'] = user.email
+        if user.firstuse_date is None:
+            user.firstuse_date = datetime.utcnow()
+            db.session.commit()
+        flash("You are now logged in", 'info')
+        return redirect(url_for('index'), code=303)
+    else:
+        if request.is_xhr:
+            return render_template('loginform.html',
+                                   loginform=form, ajax_re_register=True)
+        else:
+            flash("Please check your details and try again", 'error')
+            return index(loginform=form)
 
 
 # ---------------------------------------------------------------------------
@@ -229,6 +381,7 @@ def admin_reasons(key):
     else:
         abort(401)
 
+
 @app.route('/admin/list/<key>')
 def admin_list(key):
     if key and key in app.config['ACCESSKEY_LIST']:
@@ -240,6 +393,7 @@ def admin_list(key):
                                title=u'List of participants')
     else:
         abort(401)
+
 
 @app.route('/admin/data/<key>')
 def admin_data(key, skipreason=False):
@@ -275,23 +429,46 @@ def admin_data(key, skipreason=False):
     else:
         abort(401)
 
+
 @app.route('/admin/dnr/<key>')
 def admin_data_no_reason(key):
     return admin_data(key, skipreason=True)
 
+
+@app.route('/admin/classify/<key>', methods=['GET'])
+def admin_classifyform(key):
+    if key and key in app.config['ACCESSKEY_APPROVE']:
+        tz = timezone(app.config['TIMEZONE'])
+        return render_template('classify.html', participants=Participant.query.all(),
+                               utc=utc, tz=tz, enumerate=enumerate, key=key)
+
+
+@app.route('/admin/classify/<key>', methods=['POST'])
+def admin_classify(key):
+    if key and key in app.config['ACCESSKEY_APPROVE']:
+        p = Participant.query.get(request.form['id'])
+        if p:
+            p.category = request.form['category']
+    else:
+        abort(401)
+
+
 @app.route('/admin/approve/<key>', methods=['GET'])
-def approveform(key):
+def admin_approveform(key):
     if key and key in app.config['ACCESSKEY_APPROVE']:
         tz = timezone(app.config['TIMEZONE'])
         return render_template('approve.html', participants=Participant.query.all(),
                                utc=utc, tz=tz, enumerate=enumerate, key=key)
+    else:
+        abort(401)
+
 
 @app.route('/admin/approve/<key>', methods=['POST'])
-def approve(key):
+def admin_approve(key):
     if key and key in app.config['ACCESSKEY_APPROVE']:
         p = Participant.query.get(request.form['id'])
         if not p:
-            status = 'No such user'
+            status = "No such user"
         else:
             if 'action.undo' in request.form:
                 p.approved = False
@@ -314,7 +491,10 @@ def approve(key):
                 p.approved = True
                 status = "Tada!"
                 mailsent = False
-                # 1. Add to MailChimp
+                # 1. Make user account and activate it
+                user = makeuser(p)
+                user.active = True
+                # 2. Add to MailChimp
                 if MailChimp is not None and app.config['MAILCHIMP_API_KEY'] and app.config['MAILCHIMP_LIST_ID']:
                     mc = MailChimp(app.config['MAILCHIMP_API_KEY'])
                     try:
@@ -324,14 +504,16 @@ def approve(key):
                             merge_vars = {'FULLNAME': p.fullname,
                                           'JOBTITLE': p.jobtitle,
                                           'COMPANY': p.company,
-                                          'TWITTER': p.twitter},
+                                          'TWITTER': p.twitter,
+                                          'PRIVATEKEY': user.privatekey,
+                                          'UID': user.uid},
                             double_optin = False
                             )
                     except MailChimpError, e:
                         status = e.msg
                         if e.code == 214: # Already subscribed
                             mailsent = True
-                # 2. Send notice of approval
+                # 3. Send notice of approval
                 if not mailsent:
                     msg = Message(subject="Your registration has been approved",
                                   recipients = [p.email])
@@ -346,7 +528,55 @@ def approve(key):
         if request.is_xhr:
             return status
         else:
-            return redirect(url_for('approveform', key=key), code=303)
+            return redirect(url_for('admin_approveform', key=key), code=303)
+    else:
+        abort(401)
+
+
+# ---------------------------------------------------------------------------
+# Admin helper functions
+
+def makeuser(participant):
+    """
+    Convert a participant into a user. Returns User object.
+    """
+    user = User.query.filter_by(email=participant.email).first()
+    if user is None:
+        user = User(email=participant.email, participant=participant)
+        # These defaults don't get auto-added until the session is committed,
+        # but we need them before, so we have to manually assign values here.
+        user.privatekey = newid()
+        user.uid = newid()
+        db.session.add(user)
+    return user
+
+
+def _makeusers():
+    """
+    Helper function to create user accounts. Meant for one-time user only.
+    """
+    if MailChimp is not None and app.config['MAILCHIMP_API_KEY'] and app.config['MAILCHIMP_LIST_ID']:
+        mc = MailChimp(app.config['MAILCHIMP_API_KEY'])
+    else:
+        mc = None
+    for p in Participant.query.all():
+        if p.approved:
+            # Make user, but don't make account active
+            user = makeuser(p)
+            if mc is not None:
+                mc.listSubscribe(
+                    id = app.config['MAILCHIMP_LIST_ID'],
+                    email_address = p.email,
+                    merge_vars = {'FULLNAME': p.fullname,
+                                  'JOBTITLE': p.jobtitle,
+                                  'COMPANY': p.company,
+                                  'TWITTER': p.twitter,
+                                  'PRIVATEKEY': user.privatekey,
+                                  'UID': user.uid},
+                    double_optin = False,
+                    update_existing = True
+                    )
+    db.session.commit()
 
 
 # ---------------------------------------------------------------------------
