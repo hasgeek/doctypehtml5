0931f25bcc6ffa20a0a17255b94888363a55980e
Bibhas <iambibhas@gmail.com>
2017-05-03 16:00:04 +0530

    using updated flask import syntax

--- website.py
+++ website.py
@@ -14,8 +14,8 @@ import re
 from flask import Flask, abort, request, render_template, redirect, url_for
 from flask import flash, session, g, Response
 from werkzeug import generate_password_hash, check_password_hash, UserAgent
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from wtforms import Form, TextField, TextAreaField, PasswordField, SelectField
 from wtforms.validators import Required, Email, ValidationError
 from pytz import utc, timezone
